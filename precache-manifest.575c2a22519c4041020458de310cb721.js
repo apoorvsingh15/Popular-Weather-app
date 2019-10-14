8a1d6998dadeaeac549eb2856ea096c81b17b564
Apoorv Singh <apoorv.singh15@gmail.com>
2019-10-15 02:19:47 +0530

    Updates

---
+++ precache-manifest.575c2a22519c4041020458de310cb721.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "48329699d14857eaacfa91768194fd7c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "20dfa987836726fd440a",
+    "url": "/static/css/main.8f32edb9.chunk.css"
+  },
+  {
+    "revision": "3062b6707333af7709fe",
+    "url": "/static/js/2.b9382d86.chunk.js"
+  },
+  {
+    "revision": "20dfa987836726fd440a",
+    "url": "/static/js/main.e0debe40.chunk.js"
+  },
+  {
+    "revision": "7e97d92e0c97979d50a0",
+    "url": "/static/js/runtime-main.6f3dba6c.js"
+  }
+]);
